34f637c69b374f9c3e3e764962641520d16bdb14
Funnyd <dquesadaa2004@gmail.com>
2026-02-19 15:25:40 -0600

    Implementacion CRUD Productos

---
+++ BD/dbo/Stored Procedures/ObtenerUsuarios.sql
@@ -0,0 +1,15 @@
+﻿
+/* =========================================================
+   USUARIOS
+   ========================================================= */
+
+         CREATE PROCEDURE ObtenerUsuarios
+	AS
+	BEGIN
+
+	SET NOCOUNT ON;
+	   SELECT        Usuarios.Id, Usuarios.Correo, Usuarios.Password, Usuarios.idRol AS Rol
+	   FROM          Usuarios INNER JOIN
+							  Roles ON Usuarios.idRol = Roles.Id
+
+END
