9a4d0dc63b9c4500657f88c006c2bc2798c1fd7e
NathanCTR <157663583+NathanCTR@users.noreply.github.com>
2026-02-25 16:37:03 -0600

    Avance Restaurante

---
+++ Abstracciones/Modelos/Restaurante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Modelos
+{
+    public class Restaurante
+    {
+        [Required(ErrorMessage = "El campo ubicación es requerido")]
+        public string Ubicación { get; set; }
+
+        public class RestauranteRequest : Restaurante
+        {
+            public Guid id { get; set; }
+        }
+
+        public class RestauranteResponse : Restaurante
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
---
+++ Proyecto/Controllers/RestauranteController.cs
@@ -0,0 +1,41 @@
+﻿using Abstracciones.Interfaces.API;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestauranteController : ControllerBase, IUsuarioController
+    {
+        public Task<IActionResult> Agregar(UsuarioRequest usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Editar(Guid Id, UsuarioRequest usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Eliminar(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public Task<IActionResult> Obtener()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Obtener(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
