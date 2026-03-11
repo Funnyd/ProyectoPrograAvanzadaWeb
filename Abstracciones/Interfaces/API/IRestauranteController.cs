f657c5afea8e188ac01b90c6ee02a609ab56811c
NathanCTR <157663583+NathanCTR@users.noreply.github.com>
2026-03-11 13:46:52 -0600

    Nueva correción de RestauranteController

--- Abstracciones/Interfaces/API/IRestauranteController.cs
+++ Abstracciones/Interfaces/API/IRestauranteController.cs
@@ -1,10 +1,9 @@
-﻿using Abstracciones.Modelos;
+﻿
+using Abstracciones.Modelos;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
+
 
 namespace Abstracciones.Interfaces.API
 {
